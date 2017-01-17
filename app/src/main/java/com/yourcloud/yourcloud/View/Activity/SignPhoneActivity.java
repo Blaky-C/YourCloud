49a72648dd0ff82f42490f52d17a91496b79ac8b
ritchie <ritchie445412@gmail.com>
2017-01-17 10:49:48 +0800

    Login

--- app/src/main/java/com/yourcloud/yourcloud/View/Activity/SignPhoneActivity.java
+++ app/src/main/java/com/yourcloud/yourcloud/View/Activity/SignPhoneActivity.java
@@ -1,112 +1,97 @@
 package com.yourcloud.yourcloud.View.Activity;
 
 import android.content.Intent;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
-import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.kii.cloud.storage.KiiUser;
 import com.kii.cloud.storage.callback.KiiUserCallBack;
+import com.kii.cloud.storage.exception.app.ConflictException;
 import com.yourcloud.yourcloud.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignPhoneActivity extends AppCompatActivity {
 
+    public static String phoneNumber;
     public static String userName;
     public static String password;
 
-    @BindView(R.id.username)
-    TextInputLayout usernameWrapper;
-    @BindView(R.id.password)
-    TextInputLayout passwordWrapper;
+    @BindView(R.id.btn_signup)
+    Button mSignUp;
+    @BindView(R.id.tv_userphone)
+    TextView mPhone;
     @BindView(R.id.tv_username)
-    EditText mUserName;
+    TextView mUserName;
     @BindView(R.id.tv_password)
-    EditText mPassword;
-    @BindView(R.id.btn_login)
-    Button mLogin;
-    @BindView(R.id.tv_signup)
-    TextView mSignUp;
+    TextView mPassword;
     @BindView(R.id.container)
     CoordinatorLayout mCoordinatorLayout;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_phone);
         this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         ButterKnife.bind(this);
 
-        initView();
 
     }
 
-    private void initView() {
-        mUserName.setHint("Username");
-        mPassword.setHint("Password");
-        mSignUp.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
-        mSignUp.getPaint().setAntiAlias(true);
-
-    }
-
-    @OnClick({R.id.tv_signup, R.id.btn_login})
-    public void doLogin(View view) {
+    @OnClick(R.id.btn_signup)
+    public void doSignUp(View view) {
         switch (view.getId()) {
-            case R.id.tv_signup:
-                Intent intent = new Intent(this, SignUpActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.btn_login:
+            case R.id.btn_signup:
                 getUserInfo();
-                login();
-                Toast.makeText(this,"ssdfdsf",Toast.LENGTH_SHORT).show();
+                signUp();
                 break;
             default:
                 break;
         }
-
-
     }
-    public void getUserInfo(){
+
+    public void getUserInfo() {
+        phoneNumber = mPassword.getText().toString();
         userName = mUserName.getText().toString();
         password = mPassword.getText().toString();
 
     }
 
+    private void signUp() {
+        KiiUser user = KiiUser.createWithPhone(userName, phoneNumber);
+//        builder.setEmail(emailAddress);
+//        builder.setGlobalPhone(phoneNumber);
+//        KiiUser user = builder.build();
 
-    private void login() {
-
-        KiiUser.logIn(new KiiUserCallBack() {
+        user.register(new KiiUserCallBack() {
             @Override
-            public void onLoginCompleted(int token, KiiUser user, Exception exception) {
+            public void onRegisterCompleted(int token, KiiUser user, Exception exception) {
                 if (exception != null) {
+                    if (exception instanceof ConflictException) {
+                        Snackbar.make(mCoordinatorLayout, "该用户名已注册", Snackbar.LENGTH_SHORT)
+                                .show();
+                        return;
+                    }
 
-                    return;
-                } else {
-                    Snackbar.make(mCoordinatorLayout,"登录成功",Snackbar.LENGTH_SHORT)
+
+                }else {
+                    Snackbar.make(mCoordinatorLayout, "注册成功", Snackbar.LENGTH_SHORT)
                             .show();
+                    Intent loginIntent = new Intent(SignPhoneActivity.this, LoginActivity.class);
+                    startActivity(loginIntent);
                 }
-
             }
-        }, userName, password);
+        }, password);
 
     }
-
-
 }
--- app/src/main/java/com/yourcloud/yourcloud/View/Activity/SignUpActivity.java
+++ app/src/main/java/com/yourcloud/yourcloud/View/Activity/SignUpActivity.java
@@ -1,17 +1,27 @@
 package com.yourcloud.yourcloud.View.Activity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.yourcloud.yourcloud.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 public class SignUpActivity extends AppCompatActivity {
 
-    
+    @BindView(R.id.line1)
+    View line1;
+    @BindView(R.id.line2)
+    View line2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,11 +30,22 @@ public class SignUpActivity extends AppCompatActivity {
         this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 
         ButterKnife.bind(this);
-        initView();
+
     }
 
-    private void initView() {
-
-
+    @OnClick({R.id.line1, R.id.line2})
+    public void doSignUp(View view){
+        switch (view.getId()) {
+            case R.id.line1:
+                Intent emailIntent = new Intent(this, SignEmailActivity.class);
+                startActivity(emailIntent, ActivityOptions.makeSceneTransitionAnimation(this, line1, "signUpWithEmail").toBundle());
+                break;
+            case R.id.line2:
+                Intent phoneIntent = new Intent(this, SignPhoneActivity.class);
+                startActivity(phoneIntent, ActivityOptions.makeSceneTransitionAnimation(this, line2, "signUpWithPhone").toBundle());
+                break;
+            default:
+                break;
+        }
     }
 }
